c0eb840276dc642fd30dad55c061911e36b170c6
Mabudigital <Mabudigital@users.noreply.github.com>
2015-06-09 21:37:53 -0400

    Update repository

---
+++ www/js/db.js
@@ -0,0 +1,251 @@
+document.addEventListener("deviceready",init, false);
+var cust = {};
+cust.db = null;
+var image = "";
+//customer database
+cust.openDb = function(){
+	if(window.sqlitePlugin !== undefined){
+		cust.db = window.sqlitePlugin.openDatabase("customers_db", "1.0", "customersdb", 1000000);
+	} else {
+	// For debugging in simulator fallback to native SQL Lite	
+		cust.db = window.openDatabase("customers_db", "1.0", "customersdb", 1000000);
+	}
+}
+//create table
+cust.createTable = function(){
+	cust.db.transaction(function(tx){
+		//tx.executeSql('DROP TABLE customers');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS customers(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL, phone VARCHAR(255) NULL,email VARCHAR(255) NULL,birthday VARCHAR(255) NOT NULL,sex VARCHAR(255) NOT NULL, alergies VARCHAR(255) NULL, lastvisit VARCHAR(255) NULL, treatments VARCHAR(255) NULL, cut VARCHAR(255) NULL, color VARCHAR(255) NULL, hairtype VARCHAR(255) NULL, scalptype VARCHAR(255) NULL, products VARCHAR(255) NULL, image VARCHAR(255) NULL)');
+	});
+}
+function init(){
+	cust.openDb();
+	cust.createTable();
+	pic.openDb();
+	pic.createTable();
+	getAllTheData();
+}
+//insert record
+cust.insertRecord = function(n,p,e,bd,s,al,lv,tr,ct,cl,ht,st,pr){
+	cust.db.transaction(function(tx){
+		tx.executeSql('INSERT INTO customers(name,phone,email,birthday,sex,alergies,lastvisit,treatments,cut,color,hairtype,scalptype,products) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
+			[n,p,e,bd,s,al,lv,tr,ct,cl,ht,st,pr],
+			cust.onSuccess,
+			getAllTheData,
+			cust.OnError);
+	});
+} 
+//function will be called when process succeed
+cust.onSuccess = function (tx, r){
+	navigator.notification.alert(
+        'Done!',  // message
+        null,         // callback
+        'Salon Assistant',            // title
+        'Ok'                  // buttonName
+    );
+	window.location.replace('#customerprofile');
+}
+//function will be called when process succeed
+cust.onError = function (tx, e){
+	console.log("SQLite error: "+e.message);
+}
+//update table
+cust.updateRecord = function(n,p,e,bd,s,al,lv,tr,ct,cl,ht,st,pr,id) {
+	cust.db.transaction(function(tx) {
+		tx.executeSql("UPDATE customers SET name=?,phone=?,email=?,birthday=?,sex=?,alergies=?,lastvisit=?,treatments=?,cut=?,color=?,hairtype=?,scalptype=?,products=? WHERE id = ?",
+                 [n,p,e,bd,s,al,lv,tr,ct,cl,ht,st,pr,id],
+                 cust.onSuccess,
+                 cust.onError);
+	});
+}
+//delete record
+cust.deleteRecord = function(id) {
+	navigator.notification.confirm(
+		"Are you sure?", 
+		function(deleteButtonIndex){
+			ConfirmDelete(id,deleteButtonIndex);
+		}, 
+		"Delete Customer", 
+		"Yes,No"
+	); 
+//return false;
+};
+function ConfirmDelete(id,dstat){
+	if(dstat == "1"){
+		cust.db.transaction(function(tx){tx.executeSql("DELETE FROM customers WHERE id = ?",[id],cust.onSuccess,cust.onError);});
+		pic.deleteRecord(id);
+	}else{
+		return false;
+	};
+};
+//select all from customer_db
+cust.selectAllRecords = function(fn) {
+	cust.db.transaction(function(tx){tx.executeSql("SELECT * FROM customers", [],fn,cust.onError);});
+}
+function getAllTheData() {
+	var render = function (tx, rs) {
+	// rs contains our SQLite recordset, at this point you can do anything with it
+	// in this case we'll just loop through it and output the results to the console
+		if(rs.rows.length ==0){$('#customerlist').html("<div id='no-customer' style='text-align:center'>No customers already</div>");
+		}else{
+			for (var i = 0; i < rs.rows.length; i++) {
+				var rows = rs.rows.item(i);
+				if(image!=""){image = rows['image'];
+				}else if(rows['sex']=='Male'){image = "images/noimagemale.jpg";}else{image="images/noimagefemale.jpg";}
+				$('#customerlist').append("<li><a href='#customerdetails' onclick='getCustomerData(\""+rows['id']+"\"),getCustomerPictureData(\""+rows['id']+"\")'><img id='pic"+rows['id']+"' class='' src='"+image+"'/><div class='custlistinfo'><div class='custlistname'> "+rows['name']+"</div><div class='custlistlastvisit'>Last visit: "+rows['lastvisit']+"</div></div></a></li>");
+				$("#apnmtcustlist").append("<option value='"+rows['name']+"'>"+rows['name']+"</option>");
+				getCustomerPictureData(rows['id']);
+			}
+		}
+	}
+	cust.selectAllRecords(render);
+	
+}
+//select customer record
+cust.selectCustRecords = function(id,fd) {
+	cust.db.transaction(function(td) {
+		td.executeSql("SELECT * FROM customers WHERE id=?", [id],
+				  fd,
+				  cust.onError);
+	});
+}
+function getCustomerData(id) {
+	var image = "";
+	var renderDet = function (td, rs) {
+		// rs contains our SQLite recordset, at this point you can do anything with it
+		// in this case we'll just loop through it and output the results to the console
+		for (var i = 0; i < rs.rows.length; i++) {
+			//console.log(rs.rows.item(i));
+			var rows = rs.rows.item(0);
+			if(rows['sex']=='Male' ){image = "images/noimagemale.jpg";}else{image="images/noimagefemale.jpg";}
+			$('#custdetl #name').html("<img class='custprofimg' src='"+image+"'/> "+rows['name']);
+			$('#customerdetails #customer-info #detailname').val(rows['name']);
+			$('#customerdetails #customer-info #detailphone').val(rows['phone']);
+			$('#customerdetails #customer-info .call_btn').attr("href","tel:"+rows['phone']);
+			$('#customerdetails #customer-info #detailemail').val(rows['email']);
+			$('#customerdetails #customer-info #detailbirthday').val(rows['birthday']);
+			$('#customerdetails #customer-info #detailsex').val(rows['sex']);
+			$('#customerdetails #customer-info #detailalergies').val(rows['alergies']);
+			$('#customerdetails #customer-info #detaillastvisit').val(rows['lastvisit']);
+			$('#customerdetails #customer-info #detailtreatments').val(rows['treatments']);
+			$('#customerdetails #customer-info #detailcut').val(rows['cut']);
+			$('#customerdetails #customer-info #detailcolor').val(rows['color']);
+			$('#customerdetails #customer-info #detailhairtype').val(rows['hairtype']);
+			$('#customerdetails #customer-info #detailscalptype').val(rows['scalptype']);
+			$('#customerdetails #customer-info #detailproducts').val(rows['products']);
+			$("#piccustomerid").val(rows['id']);
+			$('#customerdetails #delete').attr("onclick","cust.deleteRecord(\""+rows['id']+"\")");
+		}
+	}
+	cust.selectCustRecords(id,renderDet);
+}
+//Pictures db
+var pic ={};
+pic.db = null;
+pic.openDb = function(){
+	if(window.sqlitePlugin !== undefined){
+	pic.db = window.sqlitePlugin.openDatabase("pictures_db", "1.0", "picturesdb", 1000000);
+	} else {
+		// For debugging in simulator fallback to native SQL Lite	
+		pic.db = window.openDatabase("pictures_db", "1.0", "picturesdb", 1000000);
+		}
+	}
+//create table
+pic.createTable = function(){
+	pic.db.transaction(function(tx){
+		//tx.executeSql('DROP TABLE customers');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS pictures(id INTEGER PRIMARY KEY AUTOINCREMENT, customerId INTEGER(255) NOT NULL, url VARCHAR(255) NULL)');
+	});
+}
+//insert record
+pic.insertRecord = function(cid,purl){
+	pic.db.transaction(function(tx){
+		tx.executeSql('INSERT INTO pictures(customerId,url) VALUES (?,?)',
+			[cid,purl],
+			pic.onSuccess,
+			pic.OnError);
+	});
+	pic.updateRecord(purl,cid);
+} 
+//function will be called when process succeed
+ pic.onSuccess = function (tx, r){
+	navigator.notification.alert(
+        'Done!',  // message
+        null,         // callback
+        'Salon Assistant',            // title
+        'Ok'                  // buttonName
+    );
+	window.location.replace('#customersdetails');
+}
+//function will be called when process succeed
+pic.onError = function (tx, e){
+    console.log("SQLite error: "+e.message);
+}
+//update table
+pic.updateRecord = function(isrc,id) {
+	pic.db.transaction(function(tx) {
+		tx.executeSql("UPDATE customer SET image =? WHERE id = ?",
+                  [isrc,id],
+                  pic.onSuccess,
+                  pic.onError);
+	});
+}
+//delete record
+pic.deleteRecord = function(id,t) {
+	navigator.notification.confirm(
+		"Are you sure?", 
+		function(deletePicButtonIndex){
+			ConfirmPicDelete(id,deletePicButtonIndex);
+		}, 
+		"Delete Picture", 
+		"Yes,No"
+	); 
+};
+function ConfirmPicDelete(id,pstat){
+	if(pstat == "1"){
+		$("li.piclist ."+id).remove();
+		pic.db.transaction(function(tx) {tx.executeSql("DELETE FROM pictures WHERE id = ?",[id],pic.onSuccess,pic.onError);});
+		
+	}else{
+		return false;
+	};
+};
+//select all from pictures_db
+pic.selectAllRecords = function(fn) {
+	pic.db.transaction(function(tx) {tx.executeSql("SELECT * FROM pictures", [],fn,pic.onError);});
+}
+function getAllPicturesData() {
+	var render = function (tx, rs) {
+		//rs contains our SQLite recordset, at this point you can do anything with it
+		//in this case we'll just loop through it and output the results to the console
+		for (var i = 0; i < rs.rows.length; i++) {
+			var picrows = rs.rows.item(i);
+				//if(picrows['url']!=""){$(".custprofimg").attr("src",picrows['url']);}else if($('#detailsex').val()=='Male'){$(".custprofimg").attr("src","images/noimagemale.jpg");}else{$(".custprofimg").attr("src","images/noimagefemale.jpg");}
+				//$('#customerimages ul').append("<li><a data-role='none' class='pic' href='"+picrows['url']+"'><img class='picbutton' src='"+picrows['url']+"' alt='pic'/></br><a id='share'data-role='none' class='share' href='#' onclick=''><img src='images/ic_action_share.png'/></a></a></li>");
+				//$(".share").attr("onclick","window.plugins.socialsharing.share('This is one of my latest work.',null,'"+picrows['url']+"')");
+		}
+	}
+	pic.selectAllRecords(render);
+}
+//select customer pictures
+pic.selectCustRecords = function(id,fd) {
+	pic.db.transaction(function(td) {td.executeSql("SELECT * FROM pictures WHERE customerId = ?", [id],fd,pic.onError);});
+}
+function getCustomerPictureData(id) {
+	var renderPic = function (td, rs) {
+	// rs contains our SQLite recordset, at this point you can do anything with it
+	// in this case we'll just loop through it and output the results to the console
+		for (var i = 0; i < rs.rows.length;i++) {
+			var rows = rs.rows.item(i);
+			if(rows['url']!=""){image = rows['url'];}else{if($('#detailsex').val()=='Male'){image = "images/noimagemale.jpg";}else{image ="images/noimagefemale.jpg";}}
+			$(".custprofimg").attr("src",image);
+			//$("#pic"+rows['id']+".custprofimg").attr("src",image);
+			$('#customerdetails #customer-info #customerimages ul').append("<li class='piclist'><a data-role='none' class='pic' href='"+rows['url']+"'><img class='picbutton' src='"+rows['url']+"' alt='"+rows['url']+"'/></br><a id='share' data-role='none' class='share' href='#' onclick=''><img src='images/ic_action_share.png'/></a><a id='picdelete' data-role='none' class='picdelete' href='#' onclick=''><img src='images/picdelete.png'/></a></a></li>");
+			$(".share").attr("onclick","window.plugins.socialsharing.share('This is one of my latest works.',null,'"+rows['url']+"')");
+			$(".picdelete").attr("onclick","pic.deleteRecord(\""+rows['id']+"\")");
+			$("li.liclist").addClass(""+rows['id']+"");
+		}
+	}
+	pic.selectCustRecords(id,renderPic);
+	
+}
---
+++ www/js/index.js
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+     takePicture: function() {
+      navigator.camera.getPicture( function( imageURI ) {
+        alert( imageURI );
+		$("#picurl").val(imageURI);
+		var cid = $("#piccustomerid").val();
+		var purl = $("#picurl").val();
+		pic.insertRecord(cid,purl);
+      },
+      function( message ) {
+        alert( message );
+      },
+      { 
+        quality : 50, 
+        saveToPhotoAlbum: true,  
+		correctOrientation: true,
+		destinationType: Camera.DestinationType.FILE_URI
+      });
+    }
+};
+function getPhoto(){            
+    navigator.camera.getPicture(onSuccess, onFail, 
+        { quality: 70, saveToPhotoAlbum: true, correctOrientation: true, destinationType: Camera.DestinationType.FILE_URI });
+
+	}
+    function onSuccess (imageURI) {
+		window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, fsFail);
+		function resolveOnSuccess(entry) {
+		var fileuri = entry.toURL();
+		//console.log(fileuri);
+		document.getElementById('image').src = fileuri;
+		$("#picurl").val(fileuri);
+		var cid = $("#piccustomerid").val();
+		var purl = $("#picurl").val();
+		//var purl = imageURI
+		pic.insertRecord(cid,purl);
+	}
+}
+function onFail (message) {
+    alert('An error Occured: ' + message);
+}
+*/
+function getPhoto(){
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 70,correctOrientation:true,destinationType: Camera.DestinationType.FILE_URI});
+
+	function onSuccess(imageURI) {
+		window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resolveOnError);
+		document.getElementById('image').src = imageURI;
+	}
+
+	function onFail(message) {
+		alert('Failed because: ' + message);
+	}
+
+	function resolveOnSuccess(fileEntry){
+		var d = new Date(),
+		n = d.getTime(),
+		newFileName = n + ".jpg";
+		var folderName = "Salon_Assistant";
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
+		  fileSys.root.getDirectory(folderName, {create:true, exclusive: false},
+			function(directory) {
+				fileEntry.copyTo(directory, newFileName,successMove,resolveOnError);
+			},
+			resolveOnError);
+		},resolveOnError);
+
+	}
+	function successMove(entry){
+		$("#picurl").val("file:///storage/emulated/0"+entry.fullPath);
+		var cid = $("#piccustomerid").val();
+		var purl = $("#picurl").val();
+		//var purl = imageURI
+		pic.insertRecord(cid,purl);
+		
+	}
+}
+function onFail(message) {
+alert('Failed to load picture because: ' + message);
+}
+function resolveOnError(error){
+	alert(error.code);
+}
